cada3dcde19856e5f233efde1a535f55e73c684a
Ben Drucker <bvdrucker@gmail.com>
2018-01-31 15:15:13 -0800

    add docker-gc

--- modules/kubernetes/helm/main.tf
+++ modules/kubernetes/helm/main.tf
@@ -1,9 +1,14 @@
+resource "helm_release" "docker-gc" {
+  name = "docker-gc"
+  chart = "stable/spotify-docker-gc"
+}
+
 resource "helm_release" "dashboard" {
   name = "dashboard"
   chart = "stable/kubernetes-dashboard"
 
   set {
     name = "rbac.create"
-    value = true
+    value = "true"
   }
 }
