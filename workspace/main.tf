20799657d5a21298c5a4d9d898c6e5466f73d726
Terraform <terraform@github.com>
2025-04-28 23:48:24 +0000

    terraform fmt

--- workspace/main.tf
+++ workspace/main.tf
@@ -2,13 +2,13 @@ resource "tfe_workspace" "this" {
   name = "infrastructure"
 
   structured_run_output_enabled = false
-  queue_all_runs = false
-  auto_apply = true
-  file_triggers_enabled = false
-  allow_destroy_plan = false
+  queue_all_runs                = false
+  auto_apply                    = true
+  file_triggers_enabled         = false
+  allow_destroy_plan            = false
 
   vcs_repo {
-    identifier = "bendrucker/infrastructure"
+    identifier                 = "bendrucker/infrastructure"
     github_app_installation_id = "ghain-fMk4yTVFAVZbgq5H"
   }
 
