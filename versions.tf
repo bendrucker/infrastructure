e46f7a9da39e519426aab44e2b2486186c996c71
Ben Drucker <bvdrucker@gmail.com>
2025-04-28 15:51:01 -0700

    provider versions

--- versions.tf
+++ versions.tf
@@ -2,10 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 2.0"
     }
+
     cloudflare = {
       source = "cloudflare/cloudflare"
+      version = "~> 1.0"
     }
   }
+
   required_version = ">= 0.13"
 }
