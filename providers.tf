561046a6459aeabbe7bfd052e5f1688e90d15d32
Ben Drucker <bvdrucker@gmail.com>
2025-04-28 15:51:42 -0700

    fmt

--- providers.tf
+++ providers.tf
@@ -3,5 +3,5 @@ provider "cloudflare" {
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
--- versions.tf
+++ versions.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 2.0"
     }
 
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "~> 1.0"
     }
   }
