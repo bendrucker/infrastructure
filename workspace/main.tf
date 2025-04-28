5a1c0a1a48b2ce94e59aa6065fb5e7406a0e6dec
Ben Drucker <bvdrucker@gmail.com>
2025-04-28 16:48:30 -0700

    use latest tf (#4)

--- workspace/main.tf
+++ workspace/main.tf
@@ -2,17 +2,17 @@ resource "tfe_workspace" "this" {
   name = "infrastructure"
 
   structured_run_output_enabled = false
-  queue_all_runs = false
-  auto_apply = true
-  file_triggers_enabled = false
-  allow_destroy_plan = false
+  queue_all_runs                = false
+  auto_apply                    = true
+  file_triggers_enabled         = false
+  allow_destroy_plan            = false
 
   vcs_repo {
-    identifier = "bendrucker/infrastructure"
+    identifier                 = "bendrucker/infrastructure"
     github_app_installation_id = "ghain-fMk4yTVFAVZbgq5H"
   }
 
-  terraform_version = "0.13.7"
+  terraform_version = "latest"
 }
 
 import {
