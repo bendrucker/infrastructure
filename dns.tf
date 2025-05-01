92f3744e6fe382cc473fd560e4af17273fb7aa1c
Ben Drucker <bvdrucker@gmail.com>
2025-05-01 13:50:16 -0700

    cloudflare: provider v1 -> v5

--- dns.tf
+++ dns.tf
@@ -1,30 +1,68 @@
 resource "cloudflare_zone" "vanity" {
-  zone = "bendrucker.me"
+  account_id = var.cloudflare_account_id
+  zone       = "bendrucker.me"
 }
 
-resource "cloudflare_record" "wwwizer" {
+resource "cloudflare_dns_record" "wwwizer" {
   zone_id = cloudflare_zone.vanity.id
   name    = cloudflare_zone.vanity.zone
   type    = "A"
   value   = "174.129.25.170"
-  ttl     = "1"
+  ttl     = 1
 }
 
-resource "cloudflare_record" "github" {
+resource "cloudflare_dns_record" "github" {
   zone_id = cloudflare_zone.vanity.id
   name    = "www"
   type    = "CNAME"
   value   = "bendrucker.github.io"
-  ttl     = "1"
+  ttl     = 1
   proxied = true
 }
 
-resource "cloudflare_record" "txt" {
+resource "cloudflare_dns_record" "txt" {
   zone_id = cloudflare_zone.vanity.id
   name    = cloudflare_zone.vanity.zone
   type    = "TXT"
-  value = join("\n", [
-    "keybase-site-verification=8ic85gbwQMRpqKksDrw_hQdsvg9WEVvX2UBvEiPHhwk"
-  ])
-  ttl = "1"
+  value   = "keybase-site-verification=8ic85gbwQMRpqKksDrw_hQdsvg9WEVvX2UBvEiPHhwk"
+  ttl     = 1
+}
+
+removed {
+  from = cloudflare_record.wwwizer
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = cloudflare_record.github
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+removed {
+  from = cloudflare_record.txt
+
+  lifecycle {
+    destroy = false
+  }
+}
+
+import {
+  to = cloudflare_dns_record.wwwizer
+  id = "c783f775892feb7781197c65222d9612/a132e35e44ae39deabc7905eb9778248"
+}
+
+import {
+  to = cloudflare_dns_record.github
+  id = "c783f775892feb7781197c65222d9612/b92197317815aba695b8ed8aa7ab2efb"
+}
+
+import {
+  to = cloudflare_dns_record.txt
+  id = "c783f775892feb7781197c65222d9612/29a2a1352dc3c1fe707beae9a9c164cd"
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "cloudflare_account_id" {
+  description = "Cloudflare account ID"
+  type        = string
+  default     = "72bdc77341dc52a3cf4a94097f9ad96f"
+}
