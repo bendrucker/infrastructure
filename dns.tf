30ceee24facfa7f20f0981412dbba07ea5b25200
Ben Drucker <bvdrucker@gmail.com>
2025-07-27 20:26:47 -0700

    dns: remove GitHub CNAME

--- dns.tf
+++ dns.tf
@@ -41,14 +41,6 @@ resource "cloudflare_ruleset" "redirects" {
   ]
 }
 
-resource "cloudflare_dns_record" "github" {
-  zone_id = cloudflare_zone.vanity.id
-  name    = "www.${cloudflare_zone.vanity.name}"
-  type    = "CNAME"
-  content = "bendrucker.github.io"
-  ttl     = 1
-  proxied = true
-}
 
 resource "cloudflare_dns_record" "txt" {
   zone_id = cloudflare_zone.vanity.id
@@ -56,14 +48,4 @@ resource "cloudflare_dns_record" "txt" {
   type    = "TXT"
   content = "keybase-site-verification=8ic85gbwQMRpqKksDrw_hQdsvg9WEVvX2UBvEiPHhwk"
   ttl     = 1
-}
-
-import {
-  to = cloudflare_dns_record.github
-  id = "c783f775892feb7781197c65222d9612/b92197317815aba695b8ed8aa7ab2efb"
-}
-
-import {
-  to = cloudflare_dns_record.txt
-  id = "c783f775892feb7781197c65222d9612/29a2a1352dc3c1fe707beae9a9c164cd"
 }
